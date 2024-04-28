090a353334a3dfea3aa652aef62c454bff8bb7db
Oksana Svishcho <oksana.svishcho@gmail.com>
2024-04-28 14:51:27 +0300

    first commit

---
+++ src/main/java/Reader.java
@@ -0,0 +1,13 @@
+import com.google.gson.Gson;
+
+public class Reader {
+    public static void main (String[] args) {
+        String name = "Oksana";
+        String lastName = "Svishcho";
+
+        Gson gson = new Gson();
+        String json = gson.toJson(new Person(name, lastName));
+
+        System.out.println(gson);
+    }
+}
